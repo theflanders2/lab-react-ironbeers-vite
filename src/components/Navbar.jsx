0ee112e219fd96a73ebfb231e86f5ef8c8cd2f49
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-02-17 13:37:31 +0100

    iteration 3 complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+import homeImg from "../assets/home-icon.png";
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <NavLink to="/"><img src={homeImg}/></NavLink>
+        </nav>
+    )
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom'
 import allBeersImg from "../assets/beers.png";
 import randomBeerImg from "../assets/random-beer.png";
 import newBeerImg from "../assets/new-beer.png";
+import Navbar from '../components/Navbar';
 
 function HomePage() {
     return (
         <div>
+            <Navbar />
+            
             <Link to="/beers">
                 <img src={allBeersImg} />
                 <br />
