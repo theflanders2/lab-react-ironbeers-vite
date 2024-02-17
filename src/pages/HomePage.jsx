7a5dbb627cc48d0d2deaa7f1cd723b876358270f
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-02-17 13:17:22 +0100

    iteration 2 complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,36 @@
-function HomePage() {}
+import { Link } from 'react-router-dom'
+import allBeersImg from "../assets/beers.png";
+import randomBeerImg from "../assets/random-beer.png";
+import newBeerImg from "../assets/new-beer.png";
+
+function HomePage() {
+    return (
+        <div>
+            <Link to="/beers">
+                <img src={allBeersImg} />
+                <br />
+                All Beers
+                <br />
+                <br />
+            </Link>
+            
+            <Link to="/random-beer">
+                <img src={randomBeerImg} />
+                <br />
+                Random Beer
+                <br />
+                <br />
+            </Link>
+            
+            <Link to="/new-beer">
+                <img src={newBeerImg} />
+                <br />
+                New Beer
+                <br />
+                <br />
+            </Link>
+        </div>
+    )
+}
 
 export default HomePage;
